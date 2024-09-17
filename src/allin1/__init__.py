1515f66bf98f5a6b0dd803350489e53a05f647ec
rzavodsky <zavodsky.richard1@gmail.com>
2024-09-17 12:29:41 +0200

    Export global values

--- src/allin1/__init__.py
+++ src/allin1/__init__.py
@@ -1,6 +1,6 @@
-from .analyze import analyze
-from .visualize import visualize
-from .sonify import sonify
-from .typings import AnalysisResult
-from .config import HARMONIX_LABELS
-from .utils import load_result
+from .analyze import analyze as analyze
+from .visualize import visualize as visualize
+from .sonify import sonify as sonify
+from .typings import AnalysisResult as AnalysisResult
+from .config import HARMONIX_LABELS as HARMONIX_LABELS
+from .utils import load_result as load_result
